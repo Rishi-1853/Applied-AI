1f0a0ef2a4e8629ed9a651e3a13920d82c189895
Rishi-1853 <rishigej72@gmail.com>
2026-06-02 15:37:23 +0530

    Add files via upload

---
+++ Day_03_DivisibleBy5and11.py
@@ -0,0 +1,7 @@
+Num = int(input("Enter a Number: "))
+
+if Num % 5 == 0 and Num % 11 == 0:
+    print(Num, "is divisible by both 5 and 11")
+else:
+    print(Num, "is NOT Divisible by both 5 and 11")
+    
