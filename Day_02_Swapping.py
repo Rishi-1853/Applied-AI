b6d6e550a06769bab4ba05e20ebbf783accd7851
Rishi-1853 <rishigej72@gmail.com>
2026-06-02 15:55:55 +0530

    Add files via upload

---
+++ Day_02_Swapping.py
@@ -0,0 +1,12 @@
+a= int(input("Enter 1st Number: "))
+b= int(input("Enter 2nd Number: "))
+
+print("\nBefore Swapping: ")
+print("a =",a)
+print("b =",b)
+
+a, b = b, a
+
+print("\nAfter Swapping: ")
+print("a =", a)
+print("b =", b)
