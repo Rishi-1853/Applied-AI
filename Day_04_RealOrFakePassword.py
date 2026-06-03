1b27a484c9b635878df78902d2bef4a7ba10a83d
Rishi-1853 <rishigej72@gmail.com>
2026-06-03 15:00:28 +0530

    Add files via upload

---
+++ Day_04_RealOrFakePassword.py
@@ -0,0 +1,6 @@
+Original_Pass= "Rishi@2005"
+User_Pass= input("Enter Your Password: ")
+if User_Pass == Original_Pass:
+    print("Correct Password Access Granted")
+else:
+    print("Wrong Password Access Denied")
